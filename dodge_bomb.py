62079080a8dc98eab1be01f0e21965b3661ef6f2
C0B23029 <c0b2302962@edu.teu.ac.jp>
2024-10-01 14:45:14 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
+import random
 import sys
 import pygame as pg
-
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -12,7 +12,6 @@ DELTA = {
     }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -20,6 +19,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb = pg.Surface((20, 20)) #空のSurface
+    bb.set_colorkey((0,0,0))
+    pg.draw.circle(bb, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb.get_rect() #爆弾rectの抽出
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx,vy = +5,-5
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -38,14 +45,16 @@ def main():
         #     sum_mv[0] -= 5
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
-        
+
         for key,tpl in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += tpl[0] #横方向
                 sum_mv[1] += tpl[1] #縦方向
 
         kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img,kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
